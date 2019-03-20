8ddc87a0ec57fb82d40382a02dac2ceb63b1e428
Sagar Shakya <shkysagar@gmail.com>
2019-03-20 22:14:15 +0545

    update header menu

--- skins/default/header.php
+++ skins/default/header.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
+          integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
     <?php wp_head(); ?>
 </head>
 <?php
@@ -12,14 +15,21 @@ $MagikCreta = new MagikCreta(); ?>
 <body <?php body_class(); ?> >
 <div id="page" class="page catalog-category-view">
 
-    <nav class="navbar navbar-expand-lg navbar-light ">
+    <nav class="navbar navbar-expand-lg ">
         <?php magikCreta_logo_image(); ?>
-        <!--        <a class="navbar-brand" href="#">CompanyName</a>-->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample01" aria-controls="navbarsExample01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse" id="navbarsExample01">
             <?php echo magikCreta_main_menu(); ?>
 
-            <ul class="navbar-nav header-user">
+            <ul class="navbar-nav header-user d-flex align-items-center">
+                <?php
+                if (class_exists('WooCommerce')) :
+                    $MagikCreta->magikCreta_mini_cart();
+                endif;
+                ?>
 
                 <?php if (is_user_logged_in()) {
                     global $current_user; ?>
@@ -44,11 +54,10 @@ $MagikCreta = new MagikCreta(); ?>
                                     <?php echo '' . esc_attr($current_user->user_email) . ''; ?>
                                 </div>
                             </div>
-                            <div class="dropdown-divider"></div>
                             <a class="dropdown-item"
                                href="<?php echo get_permalink(get_option('woocommerce_myaccount_page_id')); ?>">Action</a>
                             <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="<?php echo wp_logout_url( get_permalink() ); ?>">
+                            <a class="dropdown-item" href="<?php echo wp_logout_url(get_permalink()); ?>">
                                 Log Out
                             </a>
                         </div>
@@ -66,92 +75,7 @@ $MagikCreta = new MagikCreta(); ?>
     </nav>
 
 
-    <hr/>
 
-    <!-- Header -->
-    <header id="header">
-        <div class="header-container">
-
-            <div class="header-top">
-                <div class="container">
-                    <div class="row">
-
-                        <div class="col-xs-12 col-sm-6">
-
-                            <div class="welcome-msg">
-                                <?php echo magikCreta_msg(); ?>
-                            </div>
-                            <?php
-                            $user = wp_get_current_user();
-
-                            if ($user) :
-                                ?>
-                                <img src="<?php echo esc_url(get_avatar_url($user->ID)); ?>"/>
-                            <?php endif; ?>
-
-                        </div>
-                        <div class="col-xs-6 hidden-xs">
-                            <div class="toplinks">
-                                <div class="links">
-                                    <!-- Header Top Links -->
-                                    <?php //
-                                    //echo magikCreta_top_navigation(); ?>
-                                    <!-- End Header Top Links -->
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="container">
-                <div class="row">
-
-                    <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12 hidden-xs">
-
-                        <?php echo magikCreta_search_form(); ?>
-
-                    </div>
-
-                    <div class="col-lg-6 col-md-4 col-sm-4 col-xs-12 logo-block">
-                        <!-- Header Logo -->
-                        <div class="logo">
-                            <?php magikCreta_logo_image(); ?>
-                        </div>
-
-                        <!-- End Header Logo -->
-
-                    </div>
-                    <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                        <div class="top-cart-contain pull-right">
-                            <?php
-                            if (class_exists('WooCommerce')) :
-                                $MagikCreta->magikCreta_mini_cart();
-                            endif;
-                            ?>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    </header>
-    <nav>
-        <div class="container">
-            <div class="mm-toggle-wrap">
-                <div class="mm-toggle"><a class="mobile-toggle"><i class="fa fa-reorder"></i></a></div>
-
-            </div>
-            <div id="main-menu-new">
-                <div class="nav-inner">
-
-                    <?php echo magikCreta_main_menu(); ?>
-                </div>
-
-            </div>
-        </div>
-    </nav>
-    <!-- end header -->
     <?php if (class_exists('WooCommerce') && is_woocommerce()) : ?>
 
     <div class="breadcrumbs">
--- skins/default/homepage.php
+++ skins/default/homepage.php
@@ -1,55 +1,14 @@
 <?php global $creta_Options; ?>
 
-<div id="magik-slideshow" class="magik-slideshow">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-9">
-                <?php if (isset($creta_Options['enable_home_gallery']) && $creta_Options['enable_home_gallery'] && isset($creta_Options['home-page-slider']) && !empty($creta_Options['home-page-slider'])) { ?>
-
-                    <div id='rev_slider_4_wrapper' class='rev_slider_wrapper fullwidthbanner-container'>
-                        <div id='rev_slider_4' class='rev_slider fullwidthabanner'>
-                            <ul>
-                                <?php foreach ($creta_Options['home-page-slider'] as $slide) : ?>
-                                    <li data-transition='random' data-slotamount='7' data-masterspeed='1000'
-                                        data-thumb='<?php echo esc_url($slide['thumb']); ?>'>
-                                        <img
-                                            src="<?php echo esc_url($slide['image']); ?>"
-                                            data-bgposition='left top' data-bgfit='cover'
-                                            data-bgrepeat='no-repeat'
-                                            alt="<?php echo esc_attr($slide['title']); ?>"/> <?php echo htmlspecialchars_decode($slide['description']); ?>
-                                    </li>
-
-                                <?php endforeach; ?>
-
-
-                            </ul>
-                            <div class="tp-bannertimer"></div>
-                        </div>
-                    </div>
-
-                <?php } ?>
-            </div>
-            <div class="col-md-3 hot-deal">
-
-                <?php magikCreta_hotdeal_product(); ?>
-
-            </div>
-
-
-        </div>
-    </div>
-
-</div>
-
-<hr/>
 
 <?php //magikCreta_home_page_banner(); ?>
-<?php magikCreta_new_products();?>
-<?php magikCreta_bestseller_products(); ?>
-<?php magikCreta_featured_products();?>
-<?php magikCreta_recommended_products();?>
-<?php magikCreta_home_sub_banners ();?>
-<?php magikCreta_home_blog_posts();?>
-<?php magikCreta_home_customsection();?> 
+<?php //magikCreta_hotdeal_product(); ?>
+<?php //magikCreta_new_products();?>
+<?php //magikCreta_bestseller_products(); ?>
+<?php //magikCreta_featured_products();?>
+<?php //magikCreta_recommended_products();?>
+<?php //magikCreta_home_sub_banners ();?>
+<?php //magikCreta_home_blog_posts();?>
+<?php //magikCreta_home_customsection();?>
   
   
